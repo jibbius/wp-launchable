1b854216861239bfaa90911d6dfcca9bd78baba7
Jack Barker <jack.barker.au@gmail.com>
2014-11-28 15:57:49 +1100

    start work on AJAX handler

--- checks/Check_BlockedRobots.php
+++ checks/Check_BlockedRobots.php
@@ -11,14 +11,61 @@ class Check_BlockedRobots extends Launchable_LaunchCheck {
 			$priority = 10;
 			$alert = new Launchable_AlertMessage('<strong>Huge SEO Issue:</strong> You\'re blocking access to robots.txt');
 			$alert->suggestFix_Link('View page',admin_url('options-reading.php'));
-			$alert->suggestFix_PHPFunction('Unblock it for me', array($this->name, 'unblock_robots'));
+			$alert->suggestFix_PHPFunction('Unblock it for me', array( get_class($this) , 'unblock_robots'));
 			$this->queueAlert($alert,$priority);
+			$this->unblock_robots_client();
 		}
 
 	}
 
 	public function unblock_robots(){
+		if ( !wp_verify_nonce( $_REQUEST['nonce'], 'unblock_robots_nonce')) {
+			exit('Request not authorised');
+		}
 		update_option('blog_public','1');
+		$result['type'] = 'success';
+
+		if(!empty($_SERVER['HTTP_X_REQUESTED_WITH']) && strtolower($_SERVER['HTTP_X_REQUESTED_WITH']) == 'xmlhttprequest') {
+			$result = json_encode($result);
+			echo $result;
+		}
+		else {
+			header("Location: ".$_SERVER["HTTP_REFERER"]);
+		}
+		die();
+	}
+
+	public function unblock_robots_client(){
+		// $nonce = wp_create_nonce($function.'_nonce');
+?>
+<script>
+jQuery(document).ready( function() {
+
+   jQuery(".unblock_robots").click( function(event) {
+   	  event.preventDefault();
+   	  nonce = jQuery(this).attr("data-nonce")
+
+      jQuery.ajax({
+         type : "post",
+         dataType : "json",
+         url : <?php echo admin_url( 'admin-ajax.php' ) ?>,
+         data : {action: "unblock_robots", nonce: nonce},
+         success: function(response) {
+            if(response.type == "success") {
+               // jQuery("#unblock_robots").html(response.type)
+               alert("success")
+            }
+            else {
+               alert("failed")
+            }
+         }
+      })   
+
+   })
+
+})
+</script>
+<?php
 	}
 
 }
--- core/Launchable_AlertMessage.php
+++ core/Launchable_AlertMessage.php
@@ -20,12 +20,29 @@ class Launchable_AlertMessage {
 		$this->fixes[] = "<a href='$link' class='button'>$label</a>";
 	}
 
-	public function suggestFix_PHPFunction($label,$function){
+	public function suggestFix_PHPFunction($label,$function,$action_name){
+		//
+		// add_action("wp_ajax_".$action_name, "my_user_vote");
+		add_action("wp_ajax_nopriv_".$action_name, $function);
+
+		// Linkage mechanism
+		$if(is_array($function)){
+			$class = $function[0];
+			$action = $function[1];
+		} else {
+			$action = $function;
+		}
+		add_action( 'wp_ajax_nopriv_'.$action, $function );
+		$nonce = wp_create_nonce($function.'_nonce');
+	    $link = admin_url("admin-ajax.php?action=$function&nonce=$nonce");
+	    $link = '#';
+
 		// TODO: Implement linkage mechanism
-		$link = '#'.json_encode($function);
-		$this->fixes[] = "<a href='$link' class='button'>$label</a>";
+		// $link = '#'.json_encode($function);
+		$this->fixes[] = "<a href='$link' data-nonce='$nonce' class='button $action'>$label</a>";
 	}
-	
+
+
 	public function suggestFix_CodeSnippet($label,$snippet){
 	}
 
