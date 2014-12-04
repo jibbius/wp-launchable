becd1e45ba02abac2d548c0c2a0091a3cae1fc90
Jack Barker <jack.barker.au@gmail.com>
2014-12-04 21:58:07 +1100

    "Check for client logins" function.

--- checks/Check_ClientLogins.php
+++ checks/Check_ClientLogins.php
@@ -1,5 +1,32 @@
 <?php
 
 class Check_ClientLogins extends Launchable_LaunchCheck {
-//TODO: Implement Check_UpdatedLoginLogo
+	public function runCheck(){
+		global $wpdb;
+		$results = $wpdb->get_results('SELECT * FROM wp_users' );
+		foreach ($results as $result){
+			// Have I changed the admin password?
+			$default_password = 'password';
+			if($result->user_pass == md5($default_password)) // TODO: Test this. Not convinced there's no salting to be done.
+			{
+				//			New alert
+				$message = 'You\'re currently using the default password. That\'s very bad!';
+				$alert = new Launchable_AlertMessage($message);
+				$alert->suggestFix_Link('View page',admin_url( 'profile.php' ));
+				$this->queueAlert($alert);
+			}
+			// Have I changed the email?
+			if($result->user_email == '')
+			{
+				// TODO: Possibly include something here?
+			}
+		}
+		// Have I created client logins?
+		if (count($results) == 1){
+			$message = 'You haven\'t created any client logins';
+			$alert = new Launchable_AlertMessage($message);
+			$alert->suggestFix_Link('View page',admin_url( 'user-new.php' ));
+			$this->queueAlert($alert);
+		}
+	}
 } 
